9d89973a5442fa91211118f44859ded0870668a4
Влад Любашенко <whatislaw007@outlook.com>
2024-04-26 12:05:18 +0300

    + task 1

---
+++ hw5/index.js
@@ -0,0 +1,45 @@
+// Task 1: Advanced Array Filtering
+
+function customFilterUnique(array, getAreSame = (element1, element2) => false) {
+    if (!Array.isArray(array) || typeof getAreSame !== "function")
+        throw new Error("Incorrect args");
+    const result = [];
+    loop1: for (let element1 of array) {
+        for (let element2 of result) {
+            if (getAreSame(element1, element2)) {
+                continue loop1;
+            }
+        }
+        result.push(element1);
+    }
+    return result;
+}
+
+const humans = [
+    {
+        name: "Valera",
+        surname: "Gai",
+    },
+    {
+        name: "Galyna",
+        surname: "Shmel",
+    },
+    {
+        name: "Ivan",
+        surname: "Gai",
+    },
+    {
+        name: "Nastasia",
+        surname: "Shmel",
+    },
+    {
+        name: "Nastasia",
+        surname: "Joui",
+    },
+];
+const getAreFromOneFamily = (human1, human2) => {
+    if (human1.surname === human2.surname) return true;
+    return false;
+};
+const familyRepresentatives = customFilterUnique(humans, getAreFromOneFamily);
+console.log(familyRepresentatives);
